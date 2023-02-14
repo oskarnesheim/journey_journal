82722ba275da520403947ac8bd5ad5734cb30134
Aake Hauge <aakesh@stud.ntnu.no>
2023-02-14 14:12:05 +0100

    #2 add

--- backpacking/src/App.tsx
+++ backpacking/src/App.tsx
@@ -1,25 +1,24 @@
-import './App.css'
-import Home from './pages/Home'
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import "./App.css";
+import Home from "./pages/Home";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-import Profile from './pages/Profile'
-import About from './pages/About'
-import LoginPage from './pages/LoginPage'
-import Navbar from './components/Navbar'
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-        <BrowserRouter>
-        <Navbar/>
-            <Routes>
-                <Route path='/'  element={<LoginPage />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/home" element={<Home />} />
-            </Routes>
-        </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
-
+export default App;
--- backpacking/src/pages/Home.tsx
+++ backpacking/src/pages/Home.tsx
@@ -1,53 +1,60 @@
-import { SimpleGrid, Card, CardHeader, Heading, CardBody, CardFooter, Button } from "@chakra-ui/react";
+import {
+  SimpleGrid,
+  Card,
+  CardHeader,
+  Heading,
+  CardBody,
+  CardFooter,
+  Button,
+} from "@chakra-ui/react";
 import { getDocs } from "@firebase/firestore";
 import { useEffect, useState } from "react";
 import { Iuser } from "../interfaces/Interfaces";
 import { getCollection } from "../firebase-config";
-import '../components/components.css'
+import "../components/components.css";
 
 export default function Home() {
-    const [users, setUsers] = useState<Iuser[]>([]);
-    
-    const getUsersRef = getCollection('users');
-    
-    useEffect(() =>{
-            const getUsers = async () =>{
-            const data = await getDocs(getUsersRef)
-            setUsers(data.docs.map((person) => ({...person.data()} as Iuser)))// adds all users to the users state
-            // data.docs.map((person) => console.log({...person.data()})) // logs all users
-    }
+  const [users, setUsers] = useState<Iuser[]>([]);
+
+  const getUsersRef = getCollection("users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(getUsersRef);
+      setUsers(data.docs.map((person) => ({ ...person.data() } as Iuser))); // adds all users to the users state
+      // data.docs.map((person) => console.log({...person.data()})) // logs all users
+    };
     getUsers();
-    }, []);
+  }, []);
 
-    const showUsers = (usersIn:Iuser[]) => {
-        return (
-            usersIn.map((user) =>
-            <SimpleGrid key={user.firstname + " " + user.lastname} spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>  
-                <Card paddingBottom={4} >
-                    <CardHeader >
-                        <Heading size='md'> {user.firstname} {user.lastname}</Heading>
-                    </CardHeader>
-                    <CardBody>
-                        <p>username : {user.username}</p>
-                        <p>age : {user.age}</p>
-                    </CardBody>
-                    <CardFooter>
-                        <Button>View profile</Button>
-                    </CardFooter>
-                 </Card>
-         </SimpleGrid>
-    )
-
-    );
-    } 
-    return (
-        <div>
-            <body>
-                <div className="feed-box">
-                {showUsers(users)}
-                </div>
-
-            </body>
-        </div>
-    )
+  const showUsers = (usersIn: Iuser[]) => {
+    return usersIn.map((user) => (
+      <SimpleGrid
+        key={user.firstname + " " + user.lastname}
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+      >
+        <Card paddingBottom={4}>
+          <CardHeader>
+            <Heading size="md">
+              {" "}
+              {user.firstname} {user.lastname}
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <p>username : {user.username}</p>
+            <p>age : {user.age}</p>
+          </CardBody>
+          <CardFooter>
+            <Button>View profile</Button>
+          </CardFooter>
+        </Card>
+      </SimpleGrid>
+    ));
+  };
+  return (
+    <div className="container">
+      <div className="feed-box">{showUsers(users)}</div>
+    </div>
+  );
 }
