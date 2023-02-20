db645ab04e032b20972d46f25d1404982ee2a957
oskarin <oskarin@stud.ntnu.no>
2023-02-20 12:43:12 +0100

    $ 9 add - popup when clicking "edit post"

--- backpacking/src/pages/Profile.tsx
+++ backpacking/src/pages/Profile.tsx
@@ -1,5 +1,5 @@
-import { Card, CardHeader, Heading, CardBody, CardFooter, Button } from "@chakra-ui/react";
-import { getDocs, query, where } from "firebase/firestore";
+import { Card, CardHeader, Heading, CardBody, CardFooter, Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, Editable, EditablePreview, EditableTextarea } from "@chakra-ui/react";
+import { getDocs, query, where, deleteDoc, doc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import CreateJourney from "../components/CreateJourney";
@@ -10,10 +10,21 @@ import { UserState } from "../recoil/atoms";
 
 
 export default function Profile() {
-    const [newPostToggle, setNewPostToggle] = useState(false);
-    const [errorMessage, setErrorMessage] = useState<string>("");
-    const [currentUser, setCurrentUser] = useRecoilState(UserState);
-    const [userPosts, setUserPosts] = useState<Ijourney[]>([]);
+    const { isOpen, onOpen, onClose } = useDisclosure(); //? Modal
+    const [newPostToggle, setNewPostToggle] = useState(false); //? Velger om man skal lage en ny post eller ikke
+    const [errorMessage, setErrorMessage] = useState<string>("");  //? Error message
+    const [currentUser, setCurrentUser] = useRecoilState(UserState); //? Henter bruker fra recoil
+    const [userPosts, setUserPosts] = useState<Ijourney[]>([]); //? Henter alle brukerens poster
+
+    const [editTitle, setEditTitle] = useState<Iedit>({} as Iedit); //? Editable title
+    const [editDescription, setEditDescription] = useState<string>(""); //? Editable description
+    const [editCost, setEditCost] = useState<string>(""); //? Editable cost
+    const [editDistance, setEditDistance] = useState<string>(""); //? Editable distance
+
+    interface Iedit {
+        content: string;
+        prevState: null
+      }
     
     useEffect(() => {
         try{
@@ -53,6 +64,21 @@ export default function Profile() {
         }
     }
 
+    
+
+    const openModal = () => {
+        onOpen();
+
+    }
+
+    const deletePost = (journey:Ijourney) =>{
+        try {
+            deleteDoc(doc(getJourneyRef, journey.title));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     const showJourneys = () => {
         return (
             userPosts.map((journey) =>
@@ -64,9 +90,51 @@ export default function Profile() {
                         <p>Description : {journey.description}</p>
                         <p>Distance : {journey.distance}</p>
                         <p>Cost : {journey.cost}</p>
+                        <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+                            <ModalOverlay />
+                                <ModalContent>
+                                    <ModalHeader>
+                                        <Editable  
+                                            defaultValue={journey.title} 
+                                            className='border-dotted border-2 border-sky-500 rounded-md'>
+                                            <EditablePreview />
+                                            <EditableTextarea />    
+                                        </Editable>
+                                    </ModalHeader>
+                                    <ModalCloseButton />
+                                    <ModalBody>
+                                    Description:
+                                    {/* <div className="border-dotted border-2 border-sky-500 rounded-md w-full"> */}
+                                    <Editable defaultValue={journey.description} className='border-dotted border-2 border-sky-500 rounded-md'>
+                                        <EditablePreview />
+                                        <EditableTextarea />    
+                                    </Editable>
+                                    {/* </div> */}
+                                    Cost:
+                                    <Editable defaultValue={journey.cost.toString()} className='border-dotted border-2 border-sky-500 rounded-md'>
+                                        <EditablePreview />
+                                        <EditableTextarea />    
+                                    </Editable>
+                                    Distance:
+                                    <Editable defaultValue={journey.distance} className='border-dotted border-2 border-sky-500 rounded-md'>
+                                        <EditablePreview />
+                                        <EditableTextarea />    
+                                    </Editable>
+                                    </ModalBody>
+
+                                    <ModalFooter>
+                                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                        Lagre og lukk
+                                        </Button>
+                                        <Button variant='ghost'>Avbryt</Button>
+                                    </ModalFooter>
+                                </ModalContent>
+                        </Modal>
                     </CardBody>
                     <CardFooter>
-                        <Button>View journey</Button>
+                        <Button onClick={openModal}>Edit post</Button>
+                        <Button onClick={() => deletePost(journey)}>Delete</Button>
+
                     </CardFooter>
                  </Card>
             ));} 
