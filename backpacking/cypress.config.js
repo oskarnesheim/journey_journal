83bd4b4cc4f0577cf945cd6661c0a5a6f6bab658
Nirushaan Selvaratnam <nirushas@stud.ntnu.no>
2023-02-20 11:46:13 +0100

    Inital commit for cypress setup

--- backpacking/cypress.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    projectId: "y2irmi",
-    // The rest of the Cypress config options go here...
-  }
---
+++ backpacking/cypress/e2e/spec.cy.ts
@@ -0,0 +1,5 @@
+describe('My First Test', () => {
+  it('Visits the Home Page', () => {
+    cy.visit('http://localhost:5173/')
+  })
+})
