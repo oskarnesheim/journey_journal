d209bf6bdf69dfaf09d709b8f76b0a06d0f19f4e
Åke Sjursen Hauge <aakesh@stud.ntnu.no>
2023-02-20 12:10:44 +0100

    Revert "Merge branch 'dev' of..."
    This reverts commit 763e73098cf10afa4dc4ca29253147a7766daa71

---
+++ backpacking/src/interfaces/Interfaces.ts
@@ -0,0 +1,23 @@
+import { GeoPoint } from "firebase/firestore";
+
+export interface Iuser{
+    firstname: string,
+    lastname: string,
+    username: string,
+    password: string,
+    email: string,
+    age: number,
+    isAdmin : boolean,
+    description : string | undefined,
+    uid : string
+}
+
+export interface Ijourney{
+    title: string,
+    distance: string,
+    description: string,
+    cost: number,
+    uid: string,
+    journeyPath: Array<GeoPoint>,
+    journeyID : string
+}
