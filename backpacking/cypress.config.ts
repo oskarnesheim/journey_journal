7a2f8ffbb019c5e21a649be72b5e622bf357e1df
Nirushaan Selvaratnam <nirushas@stud.ntnu.no>
2023-02-21 16:27:50 +0100

    Minor tweak to e2e tests

--- backpacking/cypress.config.ts
+++ backpacking/cypress.config.ts
@@ -5,8 +5,16 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:5173',
+    baseUrl: "http://localhost:5173",
   },
+
   viewportHeight: 960,
   viewportWidth: 1536,
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
 });
