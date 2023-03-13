4bd95708bda9683cffa2e652876e730e0ea00ed7
Nirushaan Selvaratnam <nirushas@stud.ntnu.no>
2023-03-13 13:09:19 +0100

    Changes to e2e-tests for creating user as the implemantation was changed and caused different button names and div boxes to be changed, which needed changes in the test files

--- backpacking/cypress/e2e/createUser.cy.ts
+++ backpacking/cypress/e2e/createUser.cy.ts
@@ -2,31 +2,30 @@ describe('Testing user functionality', () => {
   it('Test create user', () => {
     cy.visit('http://localhost:5173/')
     cy.get('.authUser > :nth-child(3)').click()
-    cy.get('.chakra-form-control > :nth-child(2)').type("Conor")
-    cy.get('.chakra-form-control > :nth-child(4)').type("Mcgregor")
-    cy.get('.chakra-form-control > :nth-child(6)').type("Conor")
-    cy.get('.chakra-form-control > :nth-child(8)').type("Conor@gmail.com")
+    cy.get('.chakra-form-control > :nth-child(2)').type("Stan")
+    cy.get('.chakra-form-control > :nth-child(4)').type("Marsh")
+    cy.get('.chakra-form-control > :nth-child(6)').type("stan")
+    cy.get('.chakra-form-control > :nth-child(8)').type("stan@gmail.com")
     cy.get('.css-1jj9yua > :nth-child(1)').click()
-    cy.get('[type="password"]').type("lwchampion22")
+    cy.get('[type="password"]').type("stan123")
     cy.get('.chakra-button').click()
   })
 
   it('Test login', () => {
     cy.visit('http://localhost:5173/')
-    cy.get('[type="email"]').type("Conor@gmail.com")
-    cy.get('[type="password"]').type("lwchampion22")
+    cy.get('[type="email"]').type("stan@gmail.com")
+    cy.get('[type="password"]').type("stan123")
     cy.get('.chakra-form-control > button').click()
-    cy.get(':nth-child(6) > p').contains("Welcome back ")
+    cy.wait(3000);
+    cy.get(':nth-child(5) > p').contains("Welcome back Stan")
   })
 
   it('Test logout', () => {
     cy.visit('http://localhost:5173/')
-    cy.get('[type="email"]').type("Conor@gmail.com")
-    cy.get('[type="password"]').type("lwchampion22")
+    cy.get('[type="email"]').type("stan@gmail.com")
+    cy.get('[type="password"]').type("stan123")
     cy.get('.chakra-form-control > button').click()
-    cy.get(':nth-child(7) > button').click()
-    cy.get(':nth-child(6) > p').contains("You are not logged in")
-    
+    cy.get('.right-3 > button').click()
   })
 
 
