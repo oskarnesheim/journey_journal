87d4fa2722cac529dcd7cebfe4444f9dc8a80cdd
oskarin <oskarin@stud.ntnu.no>
2023-03-19 19:36:52 +0100

    $47 REFACTOR - modulorized profile

---
+++ backpacking/src/components/Profile/ShowJourneys.tsx
@@ -0,0 +1,71 @@
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
+import { Ijourney, IStoredJourney, Iuser } from "../../interfaces/Interfaces";
+import JourneyCard from "../JourneyCard";
+
+type ShowJourneysProps = {
+  userPosts: Ijourney[];
+  storedJData: IStoredJourney[];
+  users: Iuser[];
+  storedJourneys: Ijourney[];
+  anyUserPosts: boolean;
+  anyStoredJourneys: boolean;
+};
+
+export function ShowJourneys({
+  userPosts,
+  storedJourneys,
+  users,
+  anyUserPosts,
+  anyStoredJourneys,
+  storedJData,
+}: ShowJourneysProps) {
+  const journeyView = (fromList: Ijourney[]) => {
+    return Array.isArray(fromList) ? (
+      fromList.map((journey) => (
+        <JourneyCard
+          authorUsername={getAuthorName(journey)!}
+          fromWhatPage="profile"
+          key={journey.journeyID}
+          journey={journey}
+          usersThatStoredJourney={whoHaveStoredJourney(journey)}
+        />
+      ))
+    ) : (
+      <div>Her gikk noe galt.</div>
+    );
+  };
+
+  if (!anyUserPosts && !anyStoredJourneys) {
+    return <h1>No journeys of your own or stored journeys</h1>;
+  }
+
+  const whoHaveStoredJourney = (journey: Ijourney) => {
+    return storedJData
+      .filter((storedJ) => storedJ.journeyID === journey.journeyID)
+      .map((storedJ) => storedJ);
+  };
+  const getAuthorName = (journey: Ijourney) => {
+    return users.find((user) => user.uid === journey.uid)?.username;
+  };
+  return (
+    <div className="p-5 dark:text-theme-green dark:bg-theme-dark journeyOverview">
+      <h3 className="font-semibold text-xl">Overview:</h3>
+
+      <Tabs>
+        <TabList>
+          <Tab>Own Journeys</Tab>
+          <Tab>Stored Journeys</Tab>
+        </TabList>
+
+        <TabPanels className="dark:bg-theme-dark w-screen">
+          <TabPanel>
+            <div>{journeyView(userPosts)}</div>
+          </TabPanel>
+          <TabPanel>
+            <div>{journeyView(storedJourneys)}</div>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  );
+}
